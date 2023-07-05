2c51572c5e5a45dc57abade1a2ea3d39df4ecbff
Rodrigo Moura <rodrigovianasos@gmail.com>
2023-07-05 00:24:55 -0300

    feat: add recomended trips component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import TripSearch from "./components/TripSearch";
 import QuickSearch from "./components/QuickSearch";
+import RecommendedTrips from "./components/RecommendedTrips";
 
 export default function Home() {
   return (
     <div className=''>
       <TripSearch />
       <QuickSearch />
+      <RecommendedTrips />
     </div>
   );
 }
