a13bb55ddd2573f639bc10b77b153b95a0e61964
Rodrigo Moura <rodrigovianasos@gmail.com>
2023-07-06 22:32:24 -0300

    feat: add button my trips to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,12 +9,15 @@ import Link from "next/link";
 const Header = () => {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
   const { status, data } = useSession();
-  console.log(status, "status");
 
   const handleLoginClick = () => signIn();
   const handleLogoutClick = () => {
     setMenuIsOpen(false);
     signOut();
+  };
+
+  const handleMyTripsClick = () => {
+    setMenuIsOpen(!menuIsOpen);
   };
 
   const handleMenuClick = () => setMenuIsOpen(!menuIsOpen);
@@ -44,8 +47,14 @@ const Header = () => {
             alt={data.user.name!}
           />
           {menuIsOpen && (
-            <div className='z-50 absolute top-14 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center - items-center'>
-              <button className='text-primary text-sm font-semibold' onClick={handleLogoutClick}>
+            <div className='z-50 absolute top-14 left-0  w-full h-[100px] bg-white rounded-lg shadow-md flex flex-col justify-center - items-center'>
+              <Link href='/my-trips'>
+                <button className='text-primary text-sm font-semibold pb-2 border-b border-grayLighter border-solid'>
+                  Minhas viagens
+                </button>
+              </Link>
+
+              <button className='text-primary text-sm font-semibold pt-2' onClick={handleLogoutClick}>
                 Logout
               </button>
             </div>
