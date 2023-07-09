a170dc45ec2a77da8622b23788853c0cd18179c0
Rodrigo Moura <rodrigovianasos@gmail.com>
2023-07-09 10:55:54 -0300

    style: adapt search page to desktop

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -31,13 +31,15 @@ const Trips = () => {
   }, []);
 
   return (
-    <div className='container mx-auto flex flex-col p-5 items-center'>
-      <h1 className='text-primaryDarker font-semibold text-xl'>Hospedagens encontradas</h1>
-      <h2 className='text-grayPrimary font-medium mb-5'>
+    <div className='container mx-auto flex flex-col p-5 items-center lg:items-start'>
+      <h1 className='text-primaryDarker font-semibold text-xl lg:text-left lg:w-full lg:text-[2.5rem]'>
+        Hospedagens encontradas
+      </h1>
+      <h2 className='text-grayPrimary font-medium mb-5 lg:mt-6 lg:w-full lg:text-left'>
         {trips.length > 0 ? "Listamos as melhores viagens para você!" : "Não encontramos nenhuma viagem."}
       </h2>
 
-      <div className='flex flex-col pag-4'>
+      <div className='flex flex-col pag-4 lg:grid lg:grid-cols-4 lg:gap-10 lg:mt-6 lg:pb-16'>
         {trips.map((trip) => (
           <TripItem key={trip.id} trip={trip} />
         ))}
